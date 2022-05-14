715848644f7d475c08072aa8aa8e1baa0f1a87d3
Pavel Dvorkin <pdvorkin@lineate.com>
2022-05-14 11:43:53 +0600

    complete library2

--- src/main/java/net/thumbtack/school/library/database/Database.java
+++ src/main/java/net/thumbtack/school/library/database/Database.java
@@ -4,19 +4,24 @@ import net.thumbtack.school.library.model.EmployeeLogin;
 import net.thumbtack.school.library.service.error.ServerError;
 import net.thumbtack.school.library.service.error.ServerException;
 
+import javax.sql.rowset.serial.SerialException;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.UUID;
 
 public class Database {
 
-    private final Map<String, Employee> employeesByLogin;
+    // REVU слева интерфейс, если он есть. и имя стоит поменять
+    // private Map<String, Employee> employeesByLogin; (или loginToEmployee)
+    private HashMap<String, Employee> employers;
+    // REVU скорее всего не нужен. Во всяком случае это то же, что и employers.values()
+    private HashSet<Employee> employeeRegistration;
 
     private static Database database;
 
     private Database() {
-        employeesByLogin = new HashMap<>();
+        employers = new HashMap<>();
+        employeeRegistration = new HashSet<>();
     }
 
     public static Database getDatabase(){
@@ -26,26 +31,23 @@ public class Database {
     }
 
     public void addEmployee (Employee employee) throws ServerException {
-        if(employeesByLogin.putIfAbsent(employee.getLogin(), employee) != null)
+        // REVU не надо containsKey, putIfAbsent и проверить результат
+        if(employers.containsKey(employee.getLogin())){
             throw new ServerException(ServerError.USER_ALREADY_EXIST);
-
-//        if(employeesByLogin.containsKey(employee.getLogin())){
-//            throw new ServerException(ServerError.USER_ALREADY_EXIST);
-//        }
-//        employeesByLogin.put(employee.getLogin(), employee);
+        }
+        employers.put(employee.getLogin(), employee);
     }
 
-    public void loginEmployee (EmployeeLogin employeeLogin) throws ServerException {
-        if(employeesByLogin.containsKey(employeeLogin.getLogin())){
-            if(employeeLogin.getPassword() != employeesByLogin.get(employeeLogin.getLogin()).getPassword())
-                throw new ServerException(ServerError.WRONG_PASSWORD);
+    public String loginEmployee (EmployeeLogin employeeLogin){
+        if(employers.containsKey(employeeLogin.getLogin())){
+            if(employeeLogin.getPassword() == employers.get(employeeLogin.getLogin()).getPassword()){
+                return UUID.randomUUID().toString();
+            }
+           else
+               return "Uncorrected password";
         }
         else
-            throw new ServerException(ServerError.EMPLOYEE_NOT_FOUND);
-    }
-
-    public Employee getEmployee(String loginEmployee){
-        return employeesByLogin.get(loginEmployee);
+            return "There is no employee with this login";
     }
 
 }
