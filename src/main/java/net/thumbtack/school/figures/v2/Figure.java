48acda9095169e49ee468ed83712105d531c84a7
михаил абрамчук <20misha98@gmail.com>
2022-10-28 14:54:39 +0600

    only library

--- src/main/java/net/thumbtack/school/figures/v2/Figure.java
+++
@@ -1,7 +0,0 @@
-package net.thumbtack.school.figures.v2;
-
-import net.thumbtack.school.iface.v2.Movable;
-
-abstract class Figure implements Movable {
-
-}
--- src/main/java/net/thumbtack/school/iface/v2/HasPerimeter.java
+++
@@ -1,5 +0,0 @@
-package net.thumbtack.school.iface.v2;
-
-public interface HasPerimeter {
-    double getPerimeter();
-}
