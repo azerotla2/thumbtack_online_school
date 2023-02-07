aea5a8000162d15ef72dfe46ea8cfa0250557631
михаил абрамчук <20misha98@gmail.com>
2023-02-07 14:13:13 +0600

    changed class server

--- src/main/java/net/thumbtack/school/servlet/Server.java
+++ src/main/java/net/thumbtack/school/servlet/Server.java
@@ -1,9 +1,11 @@
 package net.thumbtack.school.servlet;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class Server {
     private static int port = 6666;
@@ -19,5 +21,45 @@ public class Server {
             }
         }
         // executorService.shutdown();
+    }
+}
+
+class ClientServiceThread extends Thread{
+    private Socket clientSocket;
+    private int clientID;
+    public ClientServiceThread(Socket socket, int id) {
+        clientSocket = socket;
+        clientID = id;
+    }
+
+    public void run() {
+        System.out.println(
+            "Accepted Client : ID - " + clientID + " : Address - " + clientSocket.getInetAddress().getHostName());
+        try (DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+             DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream())) {
+
+            while (true) {
+                String clientCommand = in.readUTF();
+                System.out.println("Client "  + clientID + " says :" + clientCommand);
+                if (clientCommand.equalsIgnoreCase("quit")) {
+                    System.out.println("Stopping client thread for client : " + clientID);
+                    break;
+                } else {
+                    out.writeUTF(clientCommand);
+                    out.flush();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println("Closing socket");
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
